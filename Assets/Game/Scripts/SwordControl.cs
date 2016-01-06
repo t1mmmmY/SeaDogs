2262b07ea18b22e60649f3326d0d0d008ee16943
denis <dovganichd@digicode.net>
2016-01-06 01:56:14 +0100

    Blending

---
+++ Assets/Game/Scripts/SwordControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum HitType
+{
+	RightHit,
+	LeftHit
+}
+
+public class SwordControl : MonoBehaviour 
+{
+	[SerializeField] Animator userAnimator;
+	int rightHitCode = Animator.StringToHash("rightHit");
+	int leftHitCode = Animator.StringToHash("leftHit");
+	int hitAngleCode = Animator.StringToHash("hitAngle");
+
+	void Awake()
+	{
+		userAnimator = GetComponent<Animator>();
+	}
+
+	public void Hit(Vector2 swingDirection)
+	{
+		HitType hitType = HitType.RightHit;
+		if (swingDirection.x < 0)
+		{
+			hitType = HitType.LeftHit;
+		}
+		else
+		{
+			hitType = HitType.RightHit;
+		}
+
+		float angle = swingDirection.y;
+		userAnimator.SetFloat(hitAngleCode, angle);
+
+		switch (hitType)
+		{
+			case HitType.LeftHit:
+				userAnimator.SetTrigger(leftHitCode);
+				break;
+			case HitType.RightHit:
+				userAnimator.SetTrigger(rightHitCode);
+				break;
+		}
+	}
+}
---
+++ Assets/Game/Scripts/SwordUserControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SwordControl))]
+public class SwordUserControl : MonoBehaviour 
+{
+	SwordControl swordControl;
+
+	void Awake()
+	{
+		swordControl = GetComponent<SwordControl>();
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			swordControl.Hit(new Vector2(0.5f, 1f));
+		}
+	}
+
+}
