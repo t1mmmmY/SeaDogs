d3daae0bb375958e4fe0effe7ab057f8dc9cfe7a
denis <dovganichd@digicode.net>
2016-01-18 17:57:47 +0100

    Awesome camera

--- Assets/Game/Scripts/CameraController.cs
+++ Assets/Game/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
 {
 	[SerializeField] Transform target;
 	[SerializeField] Transform cameraTransform;
+	[SerializeField] Transform targetHead;
+
 	[SerializeField] float speed = 1.0f;
 	[SerializeField] float speedRotate = 1.0f;
 	[SerializeField] float sensetivity = 1.0f;
@@ -14,6 +16,7 @@ public class CameraController : MonoBehaviour
 	void LateUpdate()
 	{
 		transform.DOMove(target.position, speed);
+		cameraTransform.DOMove(targetHead.position, speed);
 
 		Vector2 rotationAngle = new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y")) * sensetivity;
 
@@ -31,30 +34,16 @@ public class CameraController : MonoBehaviour
 	float ClampAngle(float angle, float min, float max)
 	{
 
-		if (angle<90 || angle>270){       // if angle in the critic region...
-			if (angle>180) angle -= 360;  // convert all angles to -180..+180
-			if (max>180) max -= 360;
-			if (min>180) min -= 360;
+		if (angle < 90 || angle > 270)
+		{       // if angle in the critic region...
+			if (angle > 180) angle -= 360;  // convert all angles to -180..+180
+			if (max > 180) max -= 360;
+			if (min > 180) min -= 360;
 		}    
 		angle = Mathf.Clamp(angle, min, max);
-		if (angle<0) angle += 360;  // if angle negative, convert to 0..360
+		if (angle < 0) angle += 360;  // if angle negative, convert to 0..360
+
 		return angle;
 	}
 
-//	float ClampAngle(float angle, float from, float to) 
-//	{
-//		if (angle > 180) 
-//		{
-//			angle = 360 - angle;
-//		}
-//
-//		angle = Mathf.Clamp(angle, from, to);
-//
-//		if (angle < 0) 
-//		{
-//			angle = 360 + angle;
-//		}
-//
-//		return angle;
-//	}
 }
