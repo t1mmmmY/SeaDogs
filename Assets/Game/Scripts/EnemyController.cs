41b622859885e7f9d36007eea087de5b7bcb0f76
denis <dovganichd@digicode.net>
2016-02-02 01:13:06 +0100

    Add enemy controller script

--- Assets/Game/Scripts/EnemyController.cs
+++ Assets/Game/Scripts/EnemyController.cs
@@ -1,15 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
